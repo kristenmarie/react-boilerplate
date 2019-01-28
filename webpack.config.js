04b76b40572e9b0d4b42e4f18a6d403f8d77728f
Kristen Kulha <kristen.m.kulha@gmail.com>
2019-01-28 10:28:20 -0800

    Initial commit.

---
+++ webpack.config.js
@@ -0,0 +1,35 @@
+const webpack = require('webpack');
+const { resolve } = require('path');
+
+module.exports = {
+
+  entry: [
+    resolve(__dirname, "src") + "/index.jsx"
+  ],
+
+  output: {
+    filename: 'app.bundle.js',
+    path: resolve(__dirname, 'build'),
+  },
+
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: "babel-loader",
+        exclude: /node_modules/,
+        options: {
+          presets: [
+            "@babel/env",
+            "@babel/react"
+          ]
+        }
+      },
+    ],
+  }
+
+};
